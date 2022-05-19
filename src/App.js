6bcd3d2200cb322beacdd76b92f6d6b61f17da88
David Okunade <davidokunade1@gmail.com>
2022-05-19 10:54:04 +0100

    fixed the header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import Home from "./Home";
 import Header from "./Header";
 import Checkout from "./Checkout";
 import Login from "./Login";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Router>
+      
+     <BrowserRouter>
+     <Header/>
       < Routes>
-      <Route exact path="/login" element={<Login/>}/>
-      <Route exact path="/header" element={<Header/>}/>
-      <Route exact path="/checkout" element={<Checkout/>}/>
-      <Route exact path="/header" element={<Header/>}/>
-      <Route exact path="/" element={<Home/>}/>
+      <Route  path="/login" element={<Login/>}/>
+      
+      <Route path="/checkout" element={<Checkout/>}/>
+      
+      <Route path="/" element={<Home/>}/>
           </Routes>
-      </Router>
+          </BrowserRouter>
 
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -16,7 +16,7 @@ function Login() {
         auth
             .signInWithEmailAndPassword(email, password)
             .then(auth => {
-                navigate.push('/');
+                navigate('/');
             })
             .catch(error => alert(error.message))
 
@@ -29,7 +29,7 @@ function Login() {
             .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
                 if (auth) {
-                    navigate.push('/');
+                    navigate('/');
                 }
             })
             .catch(error => alert(error.message))
